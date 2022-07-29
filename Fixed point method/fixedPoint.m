993ff5018de48ff87fba3f53f3d65183d4d9eca9
JosueSaenzLacayo-UCR <82010545+JosueSaenzLacayo-UCR@users.noreply.github.com>
2022-07-29 15:17:04 -0600

    Update fixedPoint.m

--- Fixed point method/fixedPoint.m
+++ Fixed point method/fixedPoint.m
@@ -3,14 +3,14 @@
 %     phi: continuous function looking for a fixed point
 %      x0: initial approximation
 %     tol: tolerance to stop
-% iterMax: maximum number of iterations
+% itrMax: maximum number of iterations
 % Output:
 %  x: the last obtained approximation
 %  k: the number of iterations performed
-function [x, k] = fixedPoint(phi, x0, tol, iterMax)
+function [x, k] = fixedPoint(phi, x0, tol, itrMax)
     k = 0;
     err = tol + 1;
-    while k <= iterMax && err >= tol
+    while k <= itrMax && err >= tol
         x = phi( x0 );
         err = abs( x - x0 ) / abs( x );
         k = k + 1;
