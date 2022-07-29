292c0d03e03e57d9b0995a948aaf4a2ee6d37cfc
JosueSaenzLacayo-UCR <82010545+JosueSaenzLacayo-UCR@users.noreply.github.com>
2022-07-29 15:16:17 -0600

    Update taylorsen.m

--- Basic serie/taylorsen.m
+++ Basic serie/taylorsen.m
@@ -1,8 +1,8 @@
 function [s] = taylorsen(x , n)
-    suma=0;
+    sum=0;
     for k=0:n
-        suma=(-1)^(k)*(x^(2*k+1)/factorial(2*k+1))+suma;
+        sum=(-1)^(k)*(x^(2*k+1)/factorial(2*k+1))+sum;
     end
     disp(sin(x));
-    disp(suma);
+    disp(sum);
 end
